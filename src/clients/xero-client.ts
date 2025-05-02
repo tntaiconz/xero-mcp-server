d0d038a0e62897bdf3d1bfae94e2579a05f56a84
Connor Adams <connorads@users.noreply.github.com>
2025-05-02 05:38:35 +0100

    Remove duplicate `accounting.transactions` scope (#71)

--- src/clients/xero-client.ts
+++ src/clients/xero-client.ts
@@ -90,7 +90,7 @@ class CustomConnectionsXeroClient extends MCPXeroClient {
 
   public async getClientCredentialsToken(): Promise<TokenSet> {
     const scope =
-      "accounting.transactions accounting.contacts accounting.settings accounting.reports.read accounting.transactions payroll.settings payroll.employees payroll.timesheets";
+      "accounting.transactions accounting.contacts accounting.settings accounting.reports.read payroll.settings payroll.employees payroll.timesheets";
     const credentials = Buffer.from(
       `${this.clientId}:${this.clientSecret}`,
     ).toString("base64");
